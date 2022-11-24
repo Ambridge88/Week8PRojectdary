135d5ccbb57a4e69d83101e41d9690e4f28ceee1
ashjshaw <ashshaw17@gmail.com>
2022-11-24 10:27:37 +0000

    mapbox, rulebox components made

---
+++ front-end/src/components/mapbox/index.jsx
@@ -0,0 +1,10 @@
+//import mapboxgl from '!mapbox-gl';
+
+//mapboxgl.accessToken = 'pk.eyJ1IjoiYXNobGV5LXNoYXciLCJhIjoiY2xha3dkdHgxMDN6bTNwc3o2ZXFlN20waiJ9.VuJuoTPAMUl1nTL-7S79UA';
+
+export default function Mapbox(){
+    return(<section className="mapBox">
+        <h1>Ye Reet</h1>
+    </section>)
+}
+
