32f5b7f61e5a10a3fc6c4c273c5a49f48aed4328
Ambridge88 <daniellelinaker@live.co.uk>
2022-11-25 15:38:31 +0000

    update

--- front-end/src/components/playerinfo/index.jsx
+++ front-end/src/components/playerinfo/index.jsx
@@ -2,14 +2,26 @@ import './index.css'
 import image from './image2.png'
 
 export default function Playerinfo(props){
+    async function steelFunction(e) {
+
+        const id = e.target.value;
+
+        return await fetch(`http://localhost:5001/atms/${id}/grab`).then((response) => response.json())
+     
+    }
+
     return(<section className='playerinfo' >
+
     <figure>
-            <img src = {image} alt="" srcset="" />
-    </figure>
+            <img src={image} alt="" srcset="" />
+        </figure>
+        <form onSubmit={(e) => steelFunction(e)}>
+            <input type="number" min="1" max="1271" />
+            <input type="submit" value="Submit" />
+        </form>
     <section>
         {props.cashBalance}
-    </section>
+    </section></section>
+        
+)}
     
-   
-    </section>)
-}
