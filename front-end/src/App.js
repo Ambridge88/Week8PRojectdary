7622f7b130a1843714f39e02308d3c17194fd5ce
Bobby2Kills <e.rob93@yahoo.co.uk>
2022-11-25 15:00:39 +0000

    ver4.0-endpointadded

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,26 @@ import Mapbox from './components/mapbox';
 import Playerinfo from './components/playerinfo';
 import Rulebox from './components/rulebox';
 import Title from './components/title';
+import {useState} from 'react'
+
+
 function App() {
+  const [ cashBalance, setCashBalance] = useState(0)
+  const fetchReq = async() => {
+    try {
+      const response = await fetch("http://localhost:5001/robbers/1/cash", {
+        method: "GET",
+        headers: {"Content-Type": "application/json"},
+        });
+      const data = await response.json();
+      console.log(data)
+      setCashBalance(data)
+      // move data around my React app
+    }
+    catch (error) {
+      console.log(error);
+    }
+  }
   return (
 
     <section className="App">
@@ -11,7 +30,7 @@ function App() {
       <section className='main'>
      <Mapbox/>
      <Rulebox/>
-     <Playerinfo/>
+     <Playerinfo cashBalance={cashBalance}/>
      
      </section>
      
--- front-end/src/components/playerinfo/index.jsx
+++ front-end/src/components/playerinfo/index.jsx
@@ -1,11 +1,15 @@
 import './index.css'
 import image from './image2.png'
 
-export default function Playerinfo(){
+export default function Playerinfo(props){
     return(<section className='playerinfo' >
     <figure>
             <img src = {image} alt="" srcset="" />
-            </figure>
+    </figure>
+    <section>
+        {props.cashBalance}
+    </section>
+    
    
     </section>)
 }
